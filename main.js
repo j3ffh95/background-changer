0df0e971abc79a42647e9e5f6ef5ab01a4e8dca1
j3ffh95 <j3ffh95@gmail.com>
2020-10-03 16:06:41 -0400

    Created randomColor Function

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const btn = document.querySelector("#btn");
 
 btn.addEventListener("click", () => {
-  document.body.style.backgroundColor = randomColor();
+  document.body.style.background = randomColor();
 });
+
+const randomColor = () => {
+  return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
+};
